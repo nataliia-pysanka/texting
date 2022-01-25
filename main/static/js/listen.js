824b286e48c8e0819966379762b3f1efc2999ad9
Nataliia Pysanka <pysankanataliia@gmail.com>
2022-01-25 05:49:16 +0200

    calculated

--- main/static/js/listen.js
+++ main/static/js/listen.js
@@ -1,21 +1,17 @@
-const greetBtn = document.getElementById('greet_btn');
-const saveBtn = document.getElementById('save_btn');
-const loadBtn = document.getElementById('load_btn');
-
-const textArea = document.querySelector('textarea');
-
 const start_x = document.getElementById('start_x');
 const start_y = document.getElementById('start_y');
-const width = document.getElementById('width');
-const height = document.getElementById('height');
-imgPath = document.getElementById('img');
+const finish_x = document.getElementById('finish_x');
+const finish_y = document.getElementById('finish_y');
+const imgPath = document.getElementById('img');
+
+const img_width = document.getElementById('img_width');
+const img_height = document.getElementById('img_height');
 
 const allSlides = document.querySelectorAll('.slide')
 let pressed = false
 
 for (let node of document.querySelectorAll('.slide')) {
   node.addEventListener('mousedown', mouseDown)
-//  node.addEventListener('mousemove', mouseMove)
 }
 
 //const div = document.createElement('div')
@@ -27,38 +23,19 @@ function mouseDown(event) {
     if (event.target.classList.contains('active')){
         if (pressed) {
             pressed = false
-            width.value =  event.pageX - getCoords(event)['left']
-            height.value =  event.pageY - getCoords(event)['top']
+            finish_x.value =  event.pageX - getCoords(event)['left']
+            finish_y.value =  event.pageY - getCoords(event)['top']
             imgPath.value = event.target.getAttribute('datasrc')
-            console.log(imgPath.value)
-
-//            console.log(width.value)
-//            console.log(height.value)
-//            div.style.right = `${event.pageX}px`
-//            div.style.bottom = `${event.pageY}px`
-//            event.target.append(div)
-//            console.log(`right ${div.style.right}, bottom ${div.style.bottom}`)
+            img_width.value = getCoords(event)['right'] - getCoords(event)['left']
+            img_height.value = getCoords(event)['bottom'] - getCoords(event)['top']
+            console.log(`${start_x.value}, ${start_y.value}, ${finish_x.value}, ${finish_y.value}`)
+            console.log(`${img_width.value}, ${img_height.value}`)
         }else{
             pressed = true
             start_x.value =  event.pageX - getCoords(event)['left']
             start_y.value =  event.pageY - getCoords(event)['top']
-//            console.log(start_x.value)
-//            console.log(start_y.value)
         }
     }
-}
-
-function mouseMove(event) {
-//    if (event.target.classList.contains('active')){
-//        if (pressed) {
-////        console.log(getCoords(event))
-//        console.log(`event.clientX ${event.clientX}, event.clientY ${event.clientY}`)
-////        console.log(`width ${event.target.clientWidth}, event.target.clientHeight ${event.target.clientHeight}`)
-//            div.style.right = `${event.pageX}px`
-//            div.style.bottom = `${event.pageY }px`
-//            event.target.append(div)
-//        }
-//    }
 }
 
 
@@ -70,37 +47,5 @@ function getCoords(elem) {
     right: box.right + window.pageXOffset,
     bottom: box.bottom + window.pageYOffset,
     left: box.left + window.pageXOffset
-//        elem: elem.target,
-//    top: box.top,
-//    right: box.right,
-//    bottom: box.bottom,
-//    left: box.left
   };
 }
-
-//function mouseUp(event) {
-//    if (event.target.classList.contains('active')){
-//        finish_x.value = event.pageX
-//        finish_y.value = event.pageY
-//        console.log(`pageX:${event.pageX}, pageY:${event.pageY}`)
-//    }
-//}
-
-greetBtn.addEventListener('click', () => {
-//    let centerX = document.documentElement.clientWidth / 2;
-//    let centerY = document.documentElement.clientHeight / 2;
-//
-//    let elem = document.elementFromPoint(centerX, centerY);
-//
-//    elem.style.background = "red";
-//    alert(elem.tagName);
-})
-
-saveBtn.addEventListener('click', () => {
-
-})
-
-loadBtn.addEventListener('click', () => {
-
-})
-
--- main/views.py
+++ main/views.py
@@ -1,22 +1,29 @@
 from PIL import Image, ImageDraw, ImageFont
 from django.shortcuts import render
 from django import forms
+from texting.settings import STATIC_ROOT
 
 font_path = "main/fonts/Noteworthy.ttc"
+box = {'start': [], 'finish': []}
 
 
 class MessageForm(forms.Form):
-    msg = forms.CharField(widget=forms.Textarea(attrs={'placeholder': "Input greetings ..."}))
+    msg = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Input greetings ...',
+                                                       'id': 'msg'}))
     start_x = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
                                                             'id': 'start_x'}))
     start_y = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
                                                             'id': 'start_y'}))
-    width = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
-                                                          'id': 'width'}))
-    height = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
-                                                          'id': 'height'}))
+    finish_x = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
+                                                          'id': 'finish_x'}))
+    finish_y = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
+                                                          'id': 'finish_y'}))
     image_path = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
                                                           'id': 'img'}))
+    image_width = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
+                                                          'id': 'img_width'}))
+    image_height = forms.CharField(widget=forms.TextInput(attrs={'type': 'hidden',
+                                                          'id': 'img_height'}))
 
 
 def greet(request):
@@ -24,112 +31,65 @@ def greet(request):
         form = MessageForm(request.POST)
         if form.is_valid():
             msg = f'{form.cleaned_data["msg"]}'
-            start_x = float(form.cleaned_data["start_x"])
-            start_y = float(form.cleaned_data["start_y"])
-            width = float(form.cleaned_data["width"])
-            height = float(form.cleaned_data["height"])
+            start = (float(form.cleaned_data["start_x"]),
+                     float(form.cleaned_data["start_y"]))
+            finish = (float(form.cleaned_data["finish_x"]),
+                    float(form.cleaned_data["finish_y"]))
             image_path = 'static/images/' + form.cleaned_data["image_path"]
-            write_text(image_path, (start_x, start_y), width, height, font_path, msg)
+            web_size = (float(form.cleaned_data["image_width"]),
+                          float(form.cleaned_data["image_height"]))
+            write_text(image_path, start, finish, web_size, font_path, msg)
             return render(request, 'main/1.html', {})
         else:
             return render(request, 'main/index.html', {'form': form})
     return render(request, 'main/index.html', {'form': MessageForm()})
 
 
-def write_text(path, start, w_width, w_height, font_path, text):
+def resize(xy, wh, real_size, web_size):
+    x = xy[0] * real_size[0] / web_size[0]
+    y = xy[1] * real_size[1] / web_size[1]
+    w = (wh[0] - xy[0]) * real_size[0] / web_size[0]
+    h = (wh[1] - xy[1]) * real_size[1] / web_size[1]
+    return x, y, w, h
+
+
+def write_text(path, start, finish, web_size, font_path, text):
     with Image.open(path) as image:
-        y_koef = image.height / w_height
-        x_koef = image.width / w_width
+        x, y, w, h = resize(start, finish, (image.width, image.height), web_size)
+
         word = text.split(' ')
-        for size in range(8, 25):
+        for size in range(6, 30):
             index = 0
             font = ImageFont.truetype(font_path, size=size)
             y = font.getsize(font_path)[1]
             while index < len(word):
                 buffer_size = 0
-                while buffer_size <= (w_width * x_koef):
+                while buffer_size <= (w - (size * 3)):
                     buffer_size += len(word[index]) + 1
                     index += 1
                     if index == len(word):
                         break
                 y += (size * 1.5)
-            if y > (w_height * y_koef):
+            if y > h:
                 size -= 1
                 break
         draw = ImageDraw.Draw(image)
         font = ImageFont.truetype(font_path, size=size)
 
         index = 0
-        x = start[0] * x_koef
-        y = start[1] * y_koef
+        draw.rounded_rectangle([x, y, x+w, y+h], radius=5, fill='white', outline=None, width=1)
+        # draw.text((x, y), text=f'x={x}, y={y}, w={w}, h={h}', fill=0, font=font)
+        # draw.text((2*x, 2*y), text=f'start={start}, finish={finish}', fill=1, font=font)
         while index < len(word):
             buffer = ''
-            while font.getsize(buffer)[0] <= (w_width * x_koef):
+            while font.getsize(buffer)[0] <= (w - (size * 3)):
                 buffer = buffer + word[index] + ' '
                 index += 1
                 if index == len(word):
                     break
-            draw.text((x, y), text=buffer, fill=0, font=font)
+            draw.text((x + (size * 1.5), y), text=buffer, fill=0, font=font)
             y += (size * 1.5)
 
-        image.save('static/images/new.jpeg')
+        image.save(STATIC_ROOT / 'images/new.jpeg')
         return True
-
-
-# def write_text(path, text):
-#     with Image.open(path) as image:
-#         w_width = finish[0] - start[0]
-#         w_height = finish[1] - start[1]
-#         word = main_text.split(' ')
-#
-#         for size in range(8, 25):
-#             index = 0
-#             font = ImageFont.truetype(font_path, size=size)
-#             y = font.getsize(font_path)[1]
-#             while index < len(word):
-#                 buffer_size = 0
-#                 while buffer_size <= w_width:
-#                     buffer_size += len(word[index]) + 1
-#                     index += 1
-#                     if index == len(word):
-#                         break
-#                 y += (size * 1.5)
-#             if y > w_height:
-#                 size -= 1
-#                 # print(y)
-#                 break
-#         # print(size)
-#         draw = ImageDraw.Draw(image)
-#         font = ImageFont.truetype(font_path, size=size)
-#
-#         index = 0
-#
-#         x = start[0]
-#         y = start[1]
-#         while index < len(word):
-#             buffer = ''
-#             while font.getsize(buffer)[0] <= w_width:
-#                 buffer = buffer + word[index] + ' '
-#                 index += 1
-#                 if index == len(word):
-#                     break
-#             draw.text((x, y), text=buffer, fill=0, font=font)
-#             y += (size * 1.5)
-#
-#         image.save(out_file)
-#
-#
-# in_file = 'how.jpeg'
-# out_file = 'new.jpg'
-#
-# font_path = "/System/Library/Fonts/Noteworthy.ttc"
-# start = (340, 40)
-# finish = (560, 100)
-# main_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '''
-#
-#
-#
-#
-#
-#
 
