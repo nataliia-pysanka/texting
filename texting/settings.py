dca95e0198369cb557897c174ed6a9f6347bcc7c
Nataliia Pysanka <pysankanataliia@gmail.com>
2022-01-25 06:31:21 +0200

    Added files and changes required for deployment to heroku

--- texting/settings.py
+++ texting/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +83,14 @@ WSGI_APPLICATION = 'texting.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                'NAME': 'text_db',
-                'USER': 'text_admin',
-                'PASSWORD': 'text_admin_24_01_22',
-                'HOST': '127.0.0.1',
-                'PORT': '5432', }
-}
+# DATABASES = {
+#     'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#                 'NAME': 'text_db',
+#                 'USER': 'text_admin',
+#                 'PASSWORD': 'text_admin_24_01_22',
+#                 'HOST': '127.0.0.1',
+#                 'PORT': '5432', }
+# }
 
 
 
@@ -132,3 +138,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
